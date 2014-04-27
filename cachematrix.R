473797e491df437657571561be68016b991adbdf
Travis May <travismay@gmail.com>
2014-04-26 20:36:45 -0500

    Modify cachematrix.R for readability

--- cachematrix.R
+++ cachematrix.R
@@ -6,13 +6,18 @@
 makeCacheMatrix <- function(x = matrix()) {
   # Declare variable to strore inverse
   inverse <- NULL
+  
   set <- function(y) {
     x <<- y
     inverse <<- NULL
   }
+  
   get <- function() x
+  
   setinverse <- function(inv) inverse <<- inv
+  
   getinverse <- function() inverse
+  
   list(set=set, get=get, setinverse=setinverse, getinverse=getinverse)
   
 }
@@ -24,13 +29,18 @@ makeCacheMatrix <- function(x = matrix()) {
 cacheSolve <- function(x, ...) {
   ## Return a matrix that is the inverse of 'x'
   inverse <- x$getinverse()
+  
   if(!is.null(inverse)) {
     message("getting cached data")
     return(inverse)
   }
+  
   data <- x$get()
+  
   inverse <- solve(data, ...)
+  
   x$setinverse(inverse)
+  
   inverse
   
 }
